516941f7c285cd573c6bcd1686664304f1af9e1b
Aman Thapa <79891651+Aman-Thapa-23@users.noreply.github.com>
2022-12-01 20:08:50 +0545

    Last one

--- HomeService/settings.py
+++ HomeService/settings.py
@@ -147,13 +147,10 @@ LOGIN_URL = 'authentication:login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # print(STATIC_ROOT)
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
